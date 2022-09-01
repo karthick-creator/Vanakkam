37b7d3c5645cfccc807951e960e812226a1574fb
Karthick G <111891234+karthickincode@users.noreply.github.com>
2022-09-01 11:11:23 +0530

    Add files via upload

---
+++ India.java
@@ -0,0 +1,13 @@
+class India
+{
+public static void main(String[] args)
+{
+India karthick = new India();
+India askin_raj = new India();
+karthick.go();
+}
+void go()
+{
+System.out.println("Going to relative's home'");
+}
+}
